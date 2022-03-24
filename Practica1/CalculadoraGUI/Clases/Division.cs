3add0783bf8279523a33662ee5595cc27d83d2ca
Pablo Jesus Che Martinez <73145273+PabloCM03@users.noreply.github.com>
2022-03-23 22:24:56 -0600

    Hola

---
+++ Practica1/CalculadoraGUI/Clases/Division.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraGUI.Clases
+{
+    internal class Division
+    {
+
+        public String Dividir (int n1, int n2)
+        {
+            if(n2 == 0)
+            {
+                return "No se puede dividir entre 0" ;
+            }
+            else
+            {
+                int d;
+                d = n1 / n2;
+                return d.ToString();
+            }
+        }
+    }
+}
