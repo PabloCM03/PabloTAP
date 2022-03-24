7f18a320e145bdba8eb6570467bd448366d5ac8d
Pablo Jesus Che Martinez <73145273+PabloCM03@users.noreply.github.com>
2022-03-23 22:09:56 -0600

    Practica1

---
+++ Practica1/CalculadoraGUI/Clases/Multiplicacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraGUI.Clases
+{
+    internal class Multiplicacion
+    {
+        public int Multiplicar(int n1, int n2)
+        {
+            int m;
+            m = n1 * n2;
+            return m;
+        }
+    }
+}
---
+++ Practica1/CalculadoraGUI/Clases/Potencia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraGUI.Clases
+{
+    internal class Potencia
+    {
+        public int Potenciar(int n1, int n2)
+        {
+            int p;
+            double bas = n1;
+            double elevado = n2;
+
+            p = (int) Math.Pow(bas,elevado);
+
+            return p;
+        }
+    }
+}
